ac01ae5cc6523b7385177c7c73fb65edd376f2f8
Chester Riley <criley.dev@gmail.com>
2022-01-14 11:12:21 +0000

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,25 @@ provider "aws" {
     secret_key = "placeholder"
     region = "eu-west-2"
 }
+
+resource "aws_instance" "manager_node" {
+    ami = "ami-0015a39e4b7c0966f"
+    instance_type = "t2.micro"
+}
+
+resource "aws_instance" "worker_node" {
+    ami = "ami-0015a39e4b7c0966f"
+    instance_type = "t2.micro"
+}
+
+resource "aws_instance" "jenkins" {
+    ami = "ami-0015a39e4b7c0966f"
+    instance_type = "t2.medium"
+
+}
+
+resource "aws_instance" "load_balancer" {
+    ami = "ami-0015a39e4b7c0966f"
+    instance_type = "t2.micro"
+
+}
