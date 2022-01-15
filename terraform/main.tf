ab7d29f0e27c1c4336534d51c04d9ce528442f98
Chester Riley <criley.dev@gmail.com>
2022-01-15 10:52:04 +0000

    added tags to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,47 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 3.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
-}
+  }
 
 # Configure the AWS Provider
 provider "aws" {
-    profile = "dev_profile"
-    region = "eu-west-2"
+  profile = "dev_profile"
+  region  = "eu-west-2"
 }
 
 resource "aws_instance" "manager_node" {
-    ami = "ami-0015a39e4b7c0966f"
-    instance_type = "t2.micro"
+  ami           = "ami-0015a39e4b7c0966f"
+  instance_type = "t2.micro"
+    tags = {
+      Name = "Manager Node"
+  }
 }
 
 resource "aws_instance" "worker_node" {
-    ami = "ami-0015a39e4b7c0966f"
-    instance_type = "t2.micro"
+  ami           = "ami-0015a39e4b7c0966f"
+  instance_type = "t2.micro"
+    tags = {
+      Name = "Worker Node"
+  }
 }
 
 resource "aws_instance" "jenkins" {
-    ami = "ami-0015a39e4b7c0966f"
-    instance_type = "t2.medium"
+  ami           = "ami-0015a39e4b7c0966f"
+  instance_type = "t2.medium"
+    tags = {
+      Name = "CI/CD Server"
+  }
 
 }
 
 resource "aws_instance" "load_balancer" {
-    ami = "ami-0015a39e4b7c0966f"
-    instance_type = "t2.micro"
+  ami           = "ami-0015a39e4b7c0966f"
+  instance_type = "t2.micro"
+    tags = {
+      Name = "Load Balancer"
+  }
 
 }
