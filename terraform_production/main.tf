ae3f3ec26d7424efc88f4c589b8307fa2b5d40dc
Chester Riley <criley.dev@gmail.com>
2022-01-17 13:42:16 +0000

    added outputs.tf files to all subdirectories

--- terraform_production/main.tf
+++ terraform_production/main.tf
@@ -5,38 +5,40 @@ terraform {
       version = "~> 3.0"
     }
   }
+}
 
-  # Configure the AWS Provider
-  provider "aws" {
-    profile = "group_admin"
-    region  = "eu-west-2"
-    shared_credentials_file = "~/.aws/credentials"
-  }
+# Configure the AWS Provider
+provider "aws" {
+  profile                 = "group_admin"
+  region                  = "eu-west-2"
+  shared_credentials_file = "~/.aws/credentials"
+}
 
-  
-  # Define source modules for other AWS configurations
-  module "instances" {
-    source                 = "./EC2"
-  }
-  module "vpc" {
-    source         = "./VPC"
-    main_cidr_block = "10.0.0.0/24"
-  }
-  module "igw" {
-    source   = "./INTERNET_GATEWAYS"
-  }
-  module "subnet" {
-    source         = "./SUBNET"
-  }
 
-  module "security_groups" {
-    source = "./SECURITY_GROUPS"
-  }
+# Define source modules for other AWS configurations
+module "instances" {
+  source = "./EC2"
 
-  module "iam" {
-    source = "./IAM"
-  }
+}
+module "vpc" {
+  source          = "./VPC"
+  main_cidr_block = "10.0.0.0/24"
+}
+module "igw" {
+  source = "./INTERNET_GATEWAYS"
+}
+module "subnet" {
+  source = "./SUBNET"
+}
 
-  module "eks" {
-    source               = "./EKS"
-  }
+module "security_groups" {
+  source = "./SECURITY_GROUPS"
+}
+
+module "iam" {
+  source = "./IAM"
+}
+
+module "eks" {
+  source = "./EKS"
+}
