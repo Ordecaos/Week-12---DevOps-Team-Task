f2b4fd261ba77b3ba4d010a067491e2c227e3075
Chester Riley <criley.dev@gmail.com>
2022-01-17 14:00:54 +0000

    added ec2 variables

---
+++ terraform_production/EC2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "Amazon Machine Image"
+  default     = "ami-0015a39e4b7c0966f"
+}
+
+variable "instance_micro" {
+  description = "Micro EC2 Instance"
+  default     = "t2.micro"
+}
+
+variable "instance_medium" {
+  description = "Medium EC2 Instance"
+  default     = "t2.micro"
+}
+
