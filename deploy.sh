f8b805b15e4f6144c2c97a68afe51d66cd5bce8c
Chester Riley <criley.dev@gmail.com>
2022-01-17 23:18:38 +0000

    added replicas to docker-compose.yaml, deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade
+sudo apt install -y curl jq
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install -y npm
+curl https://get.docker.com | sudo bash
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+node -version
+npm -v
+docker -version
+docker-compose -v
+
+# chmod u+x deploy.sh
+#./deploy.sh
