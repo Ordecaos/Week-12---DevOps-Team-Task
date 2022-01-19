74f1f4daae43601b7ed8f8013fad88b1a7cd466e
myounis789 <mohammedyounis789@gmail.com>
2022-01-19 14:15:45 +0000

    Added instructions on how to populate mysql container in script

--- mysql.sh
+++ mysql.sh
@@ -4,8 +4,9 @@ docker pull myounis789/petclinicrest:latest
 docker pull lordkalmed/frontend:latest
 docker-compose up -d
 docker exec -it MySQL bash
-mysql -u root -p petclinic
-source data.sql
-exit
-exit
-docker ps
+
+# ---RUN THESE COMMANDS AFTER THE SCRIPT ENTERS THE MYSQL CONTAINER----
+# mysql -u root -p petclinic
+# source data.sql
+# exit
+# exit
