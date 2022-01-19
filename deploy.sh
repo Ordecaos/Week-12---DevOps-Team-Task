cf550a0ce1be1820710a5a1ec83c1f7d1f1659a5
LordKalmed <angust.cloud@gmail.com>
2022-01-19 13:52:37 +0000

    testing server deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 docker system prune -a  -f
 docker pull lordkalmed/backend:latest 
-docker pull lordkalmed/frontend:latest
+docker pull lordkalmed/frontend_test:latest
 docker pull lordkalmed/nginx_live:latest
 docker-compose up
+
+
