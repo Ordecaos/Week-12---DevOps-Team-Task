ee628cab15a23a31fcda2005e7f0c092035f5d04
myounis789 <mohammedyounis789@gmail.com>
2022-01-19 14:04:29 +0000

    Updated script to populate MySQL container

--- mysql.sh
+++ mysql.sh
@@ -3,3 +3,9 @@ docker pull lordkalmed/nginx_live:latest
 docker pull myounis789/petclinicrest:latest
 docker pull lordkalmed/frontend:latest
 docker-compose up -d
+docker exec -it MySQL bash
+mysql -u root -p petclinic
+source data.sql
+exit
+exit
+docker ps
