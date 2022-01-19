d7ffed3d02154f46442b3aabd86a7e281642d75c
myounis789 <mohammedyounis789@gmail.com>
2022-01-19 14:00:55 +0000

    Added script to run with mysql on localhost for now

---
+++ mysql.sh
@@ -0,0 +1,5 @@
+docker pull myounis789/petclinicmysql:latest
+docker pull lordkalmed/nginx_live:latest
+docker pull myounis789/petclinicrest:latest
+docker pull lordkalmed/frontend:latest
+docker-compose up -d
