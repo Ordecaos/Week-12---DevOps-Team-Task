2605b17d31c58fe4463f454782892b37428029eb
Ordecaos <jackwatkinson1996@hotmail.co.uk>
2022-01-20 13:47:46 +0000

    single run changed

--- spring-petclinic-angular/karma.conf.js
+++ spring-petclinic-angular/karma.conf.js
@@ -33,7 +33,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
 
     singleRun: true,
     reporters: ['dots', 'junit'],
