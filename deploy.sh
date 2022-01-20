1af7d61505051227f054a610ed5198d768a8c898
myounis789 <mohammedyounis789@gmail.com>
2022-01-20 11:49:54 +0000

    Updated files to work with sql

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade
+sudo apt install -y curl jq
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install -y npm
+curl https://get.docker.com | sudo bash
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 666 /var/run/docker.sock
+node --version
+npm -v
+docker --version
+docker-compose -v
