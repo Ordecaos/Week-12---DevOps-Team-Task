aad3ce5ee8b97a4642dafdca15e4d24b3e706cc0
myounis789 <mohammedyounis789@gmail.com>
2022-01-20 12:03:00 +0000

    Fixed bugs in deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,12 @@ sudo apt install -y nodejs
 sudo apt install -y npm
 curl https://get.docker.com | sudo bash
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
-node -version
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 666 /var/run/docker.sock
+node --version
 npm -v
-docker -version
+docker --version
 docker-compose -v
 
 # chmod u+x deploy.sh
